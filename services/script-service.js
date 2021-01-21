c6d09bbe1c58df8c81fd31da142c6576698aad42
Gabrielle Rosa <gabi.rosa1@hotmail.com>
2021-01-21 17:10:49 -0300

    Adicionando no script a execucao do Daily reminder e atualizar no banco de dados pro próximo dia

--- services/script-service.js
+++ services/script-service.js
@@ -1,12 +1,12 @@
-const reminderModel = require('../models/reminder-model');
+const reminderCommonModel = require('../models/reminder-model');
+const reminderDailyModel = require('../models/reminder-daily-model');
 
 const script = module.exports = {}
 
-script.execute = async function(bot) {
+script.commonReminder = async function(bot) {
 
   var currentDate = Date.now()
-  console.log(currentDate)
-  var dates = await reminderModel.find({reminder_time: { $lte: currentDate} })
+  var dates = await reminderCommonModel.find({reminder_time: { $lte: currentDate} })
 
   console.log(dates)
   //2) PEGAR OS LEMBRETES QUE JÁ PASSARAM DA HORA ATUAL
@@ -16,6 +16,35 @@ script.execute = async function(bot) {
   })
 
   //4) EXCLUIR OS ENVIADOS DO BANCO DE DADOS
-  await reminderModel.deleteMany({reminder_time: { $lte: currentDate} })
+  await reminderCommonModel.deleteMany({reminder_time: { $lte: currentDate} })
+}
 
+script.dailyReminder = async function(bot) {
+
+  var currentDateMili = Date.now()
+  var dates = await reminderDailyModel.find({reminder_time: { $lte: currentDateMili} })
+  console.log(dates)
+
+  //2) PEGAR OS LEMBRETES QUE JÁ PASSARAM DA HORA ATUAL
+  dates.forEach(async (objReminder) => {
+    //3) ENVIAR ELES POR MSG
+    bot.sendMessage(objReminder.groupId, 'Ei sua(seu) porra, ' + '[' +  objReminder.nome_pessoa + ' ]' +
+        '(tg://user?id=' + objReminder.id_pessoa + ')' + '\n' +
+        objReminder.texto,
+        {parse_mode:'Markdown'})
+
+    var currentDate = new Date(objReminder.reminder_time);
+    currentDate.setDate( currentDate.getDate() + 1 );
+
+    objReminder.reminder_time = currentDate.getTime();
+
+    //4) EDITAR NOVA DATA NO BANCO
+    await objReminder.save();
+  })
+}
+
+script.execute = async function(bot) {
+
+  this.commonReminder(bot);
+  this.dailyReminder(bot);
 }
